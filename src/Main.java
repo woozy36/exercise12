6c0af7c55f6f357e679571e30e5c42d955b2d8b1
117090 <117090@swcsd.us>
2024-04-09 14:15:33 -0400

    12

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+
+class StackFullException extends Exception {}
+
+class StackEmptyException extends Exception {}
+
+
+class Queue<E> {
+    private E[] elements;
+    private int index = 0;
+    private int size;
+
+    Queue(int size) {
+        elements = (E[]) new Object[size];
+        this.size = size;
+    }
+
+    void enqueue(E element ) throws StackFullException {
+        if (index >= size) {
+            throw new StackFullException();
+        }
+
+        elements[index] = element;
+        index++;
+    }
+
+    E dequeue () throws StackEmptyException {
+        if (index == 0) {
+            throw new StackEmptyException();
+        }
+        E returnElement = elements[index - 1];
+        index--;
+        return returnElement;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] agrs) {
+        Queue<String> strings = new Queue<>(2);
+        try {
+            strings.enqueue("Hello");
+            strings.enqueue("World");
+            System.out.println(strings.dequeue());
+            System.out.println(strings.dequeue());
+        } catch (StackFullException | StackEmptyException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
